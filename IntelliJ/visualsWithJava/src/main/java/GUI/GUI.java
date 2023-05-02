b6e269868314fbd4f67abb9a4c7ff48dc5b2b69e
kurealnum <cattledoger@gmail.com>
2023-05-02 10:52:39 -0400

    Moved to VSCODE. Frick IntellJ

--- IntelliJ/visualsWithJava/src/main/java/GUI/GUI.java
+++
@@ -1,8 +0,0 @@
-package GUI.GUI;
-
-public class GUI{
-
-    // create one Frame
-    public static void main(String[] args) {
-    }
-}
--- VSCode/javaNotes/src/main/java/basics/arraysScanners.java
+++ VSCode/javaNotes/src/main/java/basics/arraysScanners.java
@@ -2,11 +2,12 @@ package basics;
 
 //this is an import statement
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*these are the basics; i.e. data types, loops, arrays, ect.
 this whole thing is a multi line comment
 this is a class *insert down arrow here**/
-public class Basics {
+public class arraysScanners {
 
     /*this is a private array called tools that holds strings.
     you can't change the size of an array once it's instantiated
@@ -17,4 +18,9 @@ public class Basics {
                                                    instantiates a new
       Data type that said object holds | arraylist object that holds strings*/
     private ArrayList<String> paintColors = new ArrayList<String>();
+
+    /*accept user input with a scanner object*/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
