36b4c7b99e096ed016678cbd62b18e79d959b3c5
kurealnum <cattledoger@gmail.com>
2023-05-09 11:29:38 -0400

    Time stuff

--- VSCode/javaNotes/src/main/java/serialization/Deserialize.java
+++ VSCode/javaNotes/src/main/java/serialization/Deserialize.java
@@ -1,40 +1,37 @@
 package serialization;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectStreamClass;
+import java.io.*;
 
 public class Deserialize {
-    public static void main(String [] args) {
+
+   public static void main(String [] args) {
 	   
-        User user = null;
-            
-        FileInputStream fileIn;
-          try {
-              fileIn = new FileInputStream("C:\\Users\\Cakow\\eclipse-workspace\\SerializeDemo\\employee.ser");
-               ObjectInputStream in = new ObjectInputStream(fileIn);
-               user = (User) in.readObject();
-               in.close();
-               fileIn.close();
-            
-          } catch (FileNotFoundException e) {
-              e.printStackTrace();
-          } catch (IOException e) 
-              e.printStackTrace();
-          } catch (ClassNotFoundException e) {
-              e.printStackTrace();
-          }
-  
-        long serialVersionUID = ObjectStreamClass.lookup(user.getClass()).getSerialVersionUID();
-        System.out.println("serialVersionUID: "+serialVersionUID);
-            
-        System.out.println("name: " + user.name);
-        System.out.println("password: " + user.password);
-  
-        user.hello();
-        
-     
-  }
+      User user = null;
+          
+      FileInputStream fileIn;
+		try {
+			fileIn = new FileInputStream("C:\\Users\\Cakow\\eclipse-workspace\\SerializeDemo\\employee.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         user = (User) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	  long serialVersionUID = ObjectStreamClass.lookup(user.getClass()).getSerialVersionUID();
+	  System.out.println("serialVersionUID: "+serialVersionUID);
+	      
+      System.out.println("name: " + user.name);
+      System.out.println("password: " + user.password);
+
+      user.hello();
+      
+   }
 }
+
